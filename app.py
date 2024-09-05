555878df2056458296b150fcc0aa9dda2a938538
Nik++ <98247758+Nikhil011205@users.noreply.github.com>
2024-09-05 22:03:39 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
+from flask_cors import CORS
 
-# Load the Keras model
 model = tf.keras.models.load_model('linux_distribution_predictor.h5')
 
 app = Flask(__name__)
+CORS(app, resources={r"/predict": {"origins": "*"}})
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json(force=True)
-    user_input = np.array(data['input'])  # Assume data['input'] is a list of inputs
+    user_input = np.array(data['input'])  
     prediction = model.predict(user_input.reshape(1, -1))
-    top_3 = np.argsort(prediction[0])[-3:][::-1]  # Get top 3 predictions
+    top_3 = np.argsort(prediction[0])[-3:][::-1] 
     return jsonify({'top_3': top_3.tolist()})
 
 if __name__ == '__main__':
